d47411451d4152b51f9b0ec8c76df2f2479e4011
MR-Titus02 <titusroxsan@gmail.com>
2025-07-13 20:15:55 +0530

    list & tubles

--- list.py
+++ list.py
@@ -1,9 +1,21 @@
-# Create a list
+# # Create a list
 list1 = [1, 2, 3, "uki", 6, "python", 8, 9, True]
 
-list2 = list("Name")
-print(list2)
+# list2 = list("Name")
+# print(list2)
 
-#check the type
-print(type(list1))
-print(type(list2))
+# #check the type
+# print(type(list1))
+# print(type(list2))
+
+# print(list1[1:5])
+
+# print(list1[1])
+
+# list1.append(list2)
+
+# print(list1[2][2])
+
+
+for i in range(len(list1)):
+    print(i)
